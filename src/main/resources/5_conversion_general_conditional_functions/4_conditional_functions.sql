737ac6f286ea01da83228af5f1ab8eb2da3fce4c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-07-18 16:24:15 +0200

    added conditional functions

--- src/main/resources/5_conversion_general_conditional_functions/4_conditional_functions.sql
+++ src/main/resources/5_conversion_general_conditional_functions/4_conditional_functions.sql
@@ -1,1 +1,34 @@
+-- DECODE
+select COMMISSION_PCT, decode(COMMISSION_PCT, null, 0) from EMPLOYEES;
 
+-- CASE(simple)
+select case 3 * 4
+           when 12 then 100
+           end
+from DUAL;
+select case 3 * 4
+           when 12 then 'twelve'
+           end
+from DUAL;
+select case 3 * 4
+           when 11 then 'eleven'
+           when 12 then 'twelve'
+           when 24/2 then 'twelve2'
+           end
+from DUAL;
+select case 3 * 5
+           when 11 then 'eleven'
+           when 12 then 'twelve'
+           when 24/2 then 'twelve2'
+           else 'endind'
+           end
+from DUAL;
+
+-- CASE(searched)
+select case
+           when 10*1=11 then 'eleven'
+           when 'ok'='ok' then 'twelve'
+           when 24/2=12 then 'twelve2'
+           else '!!!'
+           end
+from DUAL;
